1390e3392e7b69820e1a18dcc12cfbb25ea329f3
Lucas Candido <Lucas.Pereira-Silva@tecban.com.br>
2023-10-25 17:52:40 -0300

    Add some Interceptor and maps some Cache Provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { ProductModule } from './modules/product.module';
 import { CustomerModule } from './modules/customer.module';
-import { Module, Provider } from '@nestjs/common';
+import { ClassSerializerInterceptor, Module, Provider, ValidationPipe } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ThrottlerModule } from '@nestjs/throttler';
@@ -9,13 +9,38 @@ import {
   configModuleOptions,
   throttlerModuleOptions,
   postgresTypeOrmModuleOptions,
+  cacheModuleOptions,
 } from './config';
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { CacheInterceptor, ExceptionFilter } from './core/domain/middlewares';
+import { CacheModule } from '@nestjs/cache-manager';
 
 const httpControllers = [];
-const repositories: Provider[] = [];
+const repositories: Provider[] = [
+  {
+    provide: APP_FILTER,
+    useClass: ExceptionFilter,
+  },
+  {
+    provide: APP_INTERCEPTOR,
+    useClass: CacheInterceptor,
+  },
+  {
+    provide: APP_INTERCEPTOR,
+    useClass: ClassSerializerInterceptor,
+  },
+  {
+    provide: APP_PIPE,
+    useValue: new ValidationPipe({
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  },
+];
 
 @Module({
   imports: [
+    CacheModule.register(cacheModuleOptions),
     ConfigModule.forRoot(configModuleOptions),
     ThrottlerModule.forRootAsync(throttlerModuleOptions),
     TypeOrmModule.forRootAsync(postgresTypeOrmModuleOptions),

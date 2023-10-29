afcd38c675a92f92638269e0401ddcf0fea93a3c
Lucas Candido <Lucas.Pereira-Silva@tecban.com.br>
2023-10-29 11:23:43 -0300

    Add adjustment when opening payment

--- src/core/application/payment/payment.use-case.ts
+++ src/core/application/payment/payment.use-case.ts
@@ -1,8 +1,8 @@
-import { ORDER_REPOSITORY, PAYMENT_REPOSITORY } from '@/config';
+import { PAYMENT_REPOSITORY } from '@/config';
 import { CreatePaymentDTO } from '@/core/domain/dto';
 import { Payment } from '@/core/domain/entities';
 import { IPayment, IPaymentUseCase } from '@/core/domain/interfaces';
-import { IOrderRepositoryPort, IPaymentRepositoryPort } from '@/core/domain/repositories';
+import { IPaymentRepositoryPort } from '@/core/domain/repositories';
 import { Inject, Injectable } from '@nestjs/common';
 
 @Injectable()
@@ -10,8 +10,6 @@ export class PaymentUseCase implements IPaymentUseCase {
   constructor(
     @Inject(PAYMENT_REPOSITORY)
     private readonly _paymentRepository: IPaymentRepositoryPort,
-    @Inject(ORDER_REPOSITORY)
-    private readonly _orderRepository: IOrderRepositoryPort,
   ) {}
 
   async findAll(): Promise<IPayment[]> {
@@ -19,9 +17,7 @@ export class PaymentUseCase implements IPaymentUseCase {
   }
 
   async createPayment({ order, paymentMethod }: CreatePaymentDTO): Promise<Payment> {
-    const orderFound = await this._orderRepository.findOneById(order.id);
-
-    const payment = Payment.create({ order: orderFound, paymentMethod });
+    const payment = Payment.create({ order: order, paymentMethod });
 
     return this._paymentRepository.insert(payment);
   }

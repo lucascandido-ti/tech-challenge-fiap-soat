519f077db7903077a1b80adcc3c5e7ca1e0d595a
Lucas Candido <Lucas.Pereira-Silva@tecban.com.br>
2023-10-30 20:53:32 -0300

    Update CustomerUseCase

--- src/core/application/customer/use-cases/customer.use-case.ts
+++ src/core/application/customer/use-cases/customer.use-case.ts
@@ -25,6 +25,21 @@ export class CustomerUseCase implements ICustomerUseCase {
 
   async createCustomer({ cpf, email, name }: CreateCustomerDTO): Promise<ICustomer> {
     const formatCPF = String(cpf).replace(/[^a-zA-Z0-9]/g, '');
+
+    const findCustomerByCPF = await this._customerRespository.getCustomersBy({
+      skip: 0,
+      take: 10,
+      search: formatCPF,
+    });
+    if (findCustomerByCPF.total !== 0) return findCustomerByCPF.data[0];
+
+    const findCustomerByEmail = await this._customerRespository.getCustomersBy({
+      skip: 0,
+      take: 10,
+      search: email,
+    });
+    if (findCustomerByEmail.total !== 0) return findCustomerByEmail.data[0];
+
     const customer = Customer.create({ cpf: formatCPF, email, name });
     return await this._customerRespository.insert(customer);
   }

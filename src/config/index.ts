35fda49e950367bb7756ab083a57ba8cfdfad371
Lucas Candido <Lucas.Pereira-Silva@tecban.com.br>
2024-01-18 20:37:44 -0300

    applies improvements to the configuration loading function; If you don't find the configMap, look for the local configuration file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 import { IsInstance, ValidateNested } from 'class-validator';
 
-import path from 'path';
-import fs from 'fs';
+import * as fs from 'fs';
+import * as path from 'path';
 import * as _ from 'lodash';
 
 import { DbConfig } from './db.config';
@@ -9,6 +9,7 @@ import { ApiConfig } from './api.config';
 import { getConfigModuleOptions } from './utils';
 
 import defaultSettingsJson from './settings.json';
+
 import { ConfigService } from '@nestjs/config';
 
 export * from './utils';
@@ -31,14 +32,13 @@ export function getConfigJsonConfigMap(configService: ConfigService): Record<str
   try {
     const configJsonString = configService.get<string>('MY_SETTINGS');
     const configJson = JSON.parse(configJsonString);
-
     return _.merge(defaultSettingsJson, configJson);
   } catch {
-    return defaultSettingsJson;
+    return getConfigJsonLocal();
   }
 }
 
-export function getConfigJsonLocalHost(): Record<string, unknown> {
+export function getConfigJsonLocal(): Record<string, unknown> {
   try {
     const configJsonPath = path.join(process.cwd(), '/src/config/settings.json');
     const configJsonString = fs.readFileSync(configJsonPath, {
@@ -55,7 +55,4 @@ export function getConfigJsonLocalHost(): Record<string, unknown> {
 
 // Se for testar localmente, alterar a função de captura de configurações para getConfigJsonLocalHost
 
-export const configModuleOptions = getConfigModuleOptions(
-  Config,
-  getConfigJsonConfigMap(new ConfigService()),
-);
+export const configModuleOptions = getConfigModuleOptions(Config, getConfigJsonLocal());
